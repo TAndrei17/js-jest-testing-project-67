e03c67aea44cae38b2921f9277ad97a0085210eb
Andrei Trunkin <inboxavt@gmail.com>
2023-08-13 23:11:41 +0200

    Created test changeLinks

---
+++ __tests__/change_links.test..js
@@ -0,0 +1,22 @@
+import { fileURLToPath } from 'url';
+import os from 'os';
+import path from 'path';
+import fs from 'fs/promises';
+import changeImgLimks from '../src/change_links.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const tmpFilePath = path.join(os.tmpdir(), 'tmp.html');
+
+beforeEach(async () => {
+  await fs.copyFile(createPath('before.html'), tmpFilePath);
+});
+
+test('changeHTML', async () => {
+  await changeImgLimks(tmpFilePath); // функция меняет файл, не нужно сохранять в константу!
+  const result = await fs.readFile(tmpFilePath, 'utf-8');
+  const controlHTML = await fs.readFile(createPath('after.html'), 'utf-8');
+  expect(result).toEqual(controlHTML);
+});
