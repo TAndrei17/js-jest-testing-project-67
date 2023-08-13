32d9207589209bb6c5fc403547da9f951a82abd8
Andrei Trunkin <inboxavt@gmail.com>
2023-08-13 23:11:06 +0200

    Updated test createName

--- __tests__/create_name.test.js
+++ __tests__/create_name.test.js
@@ -1,8 +1,13 @@
 import { test, expect } from '@jest/globals';
-import createFileName from '../src/create_name.js';
+import createName from '../src/create_name.js';
+
+const url = 'https://ru.hexlet.io/courses';
+const url500 = 'https://ru.hexlet.io/courses500';
 
 test('create file name', () => {
-  expect(createFileName('https://ru.hexlet.io/courses')).toEqual('ru-hexlet-io-courses.html');
-  expect(createFileName('https://ru.hexlet.io/courses500')).toEqual('ru-hexlet-io-courses500.html');
-  expect(createFileName('')).toEqual('');
+  expect(createName(url, 'html')).toEqual('ru-hexlet-io-courses.html');
+  expect(createName(url500, 'html')).toEqual('ru-hexlet-io-courses500.html');
+  expect(createName(url, 'files')).toEqual('ru-hexlet-io-courses_files');
+  expect(createName(url500, 'files')).toEqual('ru-hexlet-io-courses500_files');
+  expect(createName('')).toEqual('');
 });
