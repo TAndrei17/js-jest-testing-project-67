6b48867fb2987c255b6f4e853155153a65d5af9e
Andrei Trunkin <inboxavt@gmail.com>
2023-08-13 23:07:22 +0200

    Modified createName

--- src/create_name.js
+++ src/create_name.js
@@ -1,10 +1,15 @@
-const createFileName = (url) => {
+const endPath = {
+  html: () => '.html',
+  files: () => '_files',
+};
+
+const createName = (url, ending) => {
   if (url === '') {
     return '';
   }
   const getName = url.split('//');
   const changeSymbols = getName[1].replace(/[^a-zA-Z0-9]/g, '-');
-  return `${changeSymbols}.html`;
+  return `${changeSymbols}${endPath[ending]()}`;
 };
 
-export default createFileName;
+export default createName;
