8cf228377aa64292b94a898c8f4791e6a526d47a
Andrei Trunkin <inboxavt@gmail.com>
2023-08-17 16:00:20 +0200

    Updated function & tests of createName

--- __tests__/create_name.test.js
+++ __tests__/create_name.test.js
@@ -1,13 +1,16 @@
+import { URL } from 'url';
 import { test, expect } from '@jest/globals';
 import createName from '../src/create_name.js';
 
-const url = 'https://ru.hexlet.io/courses';
-const url500 = 'https://ru.hexlet.io/courses500';
+const url = new URL('https://ru.hexlet.io/courses');
+const url500 = new URL('https://ru.hexlet.io/courses500');
+const src = new URL('/assets/professions/nodejs.png', 'https://ru.hexlet.io');
 
 test('create file name', () => {
   expect(createName(url, 'html')).toEqual('ru-hexlet-io-courses.html');
   expect(createName(url500, 'html')).toEqual('ru-hexlet-io-courses500.html');
   expect(createName(url, 'files')).toEqual('ru-hexlet-io-courses_files');
   expect(createName(url500, 'files')).toEqual('ru-hexlet-io-courses500_files');
+  expect(createName(src)).toEqual('ru-hexlet-io-assets-professions-nodejs.png');
   expect(createName('')).toEqual('');
 });
--- src/create_name.js
+++ src/create_name.js
@@ -1,17 +1,38 @@
 const mapping = {
   html: () => '.html',
   files: () => '_files',
-  jpg: () => '.jpg',
-  jpeg: () => '.jpeg',
+  pathname: '',
+  filename: '',
 };
 
-const createName = (url, ending) => {
+const getFileName = (string) => {
+  const pathnameToArray = string.split('/');
+  const lastPartArray = pathnameToArray[pathnameToArray.length - 1];
+  const isFile = lastPartArray.includes('.');
+  if (isFile) {
+    mapping.pathname = pathnameToArray.filter((item) => item !== lastPartArray).join('/');
+    mapping.filename = lastPartArray;
+    return lastPartArray;
+  }
+  return '';
+};
+
+const createName = (url, ending = '') => {
+  const { hostname, pathname } = url;
   if (url === '') {
     return '';
   }
-  const getName = url.split('//');
-  const changeSymbols = getName[1].replace(/[^a-zA-Z0-9]/g, '-');
-  return `${changeSymbols}${mapping[ending]()}`;
+  const fileName = getFileName(pathname);
+
+  const changeHostname = hostname.replace(/[^a-zA-Z0-9]/g, '-');
+  let changePathname = pathname.replace(/[^a-zA-Z0-9]/g, '-');
+
+  if (fileName !== '') {
+    changePathname = mapping.pathname.replace(/[^a-zA-Z0-9]/g, '-');
+    return `${changeHostname}${changePathname}-${mapping.filename}`;
+  }
+
+  return `${changeHostname}${changePathname}${mapping[ending]()}`;
 };
 
 export default createName;
