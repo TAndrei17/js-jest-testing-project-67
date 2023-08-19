79c04b616f60390fb6537d960a973d582e4e73ea
Andrei Trunkin <inboxavt@gmail.com>
2023-08-19 15:48:29 +0200

    Updated test saveImagine

--- __tests__/save_imagine.test.js
+++ __tests__/save_imagine.test.js
@@ -3,22 +3,27 @@ import path from 'path';
 import { URL } from 'url';
 import nock from 'nock';
 import axios from 'axios';
+import { beforeEach } from '@jest/globals';
 
 import saveImagine from '../src/save_imagine.js';
 import createName from '../src/create_name.js';
 
-const urlLink = 'https://github.com/TAndrei17/cv_Andrei_Trunkin/Avatar_AT.jpeg';
+let tmpFilePath;
+const urlLink = 'http://www.huvila.net/650/kuvax-9909-650-S-0_iso.jpg';
 const url = new URL(urlLink);
-const fileName = createName(url);
-const tmpFilePath = process.env.GITHUB_ACTIONS_TMP_PATH
-  ? path.join(process.env.GITHUB_ACTIONS_TMP_PATH, fileName)
-  : path.join(os.tmpdir(), fileName);
+
+beforeEach(() => {
+  const fileName = createName(url);
+  tmpFilePath = process.env.GITHUB_ACTIONS_TMP_PATH
+    ? path.join(process.env.GITHUB_ACTIONS_TMP_PATH, fileName)
+    : path.join(os.tmpdir(), fileName);
+});
 
 nock.disableNetConnect();
 test('Check the request', async () => {
-  const scope = nock('https://github.com')
-    .get('/TAndrei17/cv_Andrei_Trunkin/Avatar_AT.jpeg')
-    .replyWithFile(200, tmpFilePath, { 'Content-Type': 'image/jpeg' });
+  const scope = nock('http://www.huvila.net')
+    .get('/650/kuvax-9909-650-S-0_iso.jpg')
+    .replyWithFile(200, tmpFilePath, { 'Content-Type': 'image/jpg' });
 
   await saveImagine(url, axios, tmpFilePath);
   expect(scope.isDone()).toBe(true);
