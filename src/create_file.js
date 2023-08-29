d2ccf075dd5ae78fc54490c1d78a8269ae549212
Andrei Trunkin <inboxavt@gmail.com>
2023-08-29 19:39:04 +0200

    Updated function createFile

--- src/create_file.js
+++ src/create_file.js
@@ -3,10 +3,10 @@ import fs from 'fs/promises';
 const createFileHtml = async (path, context) => {
   try {
     await fs.writeFile(path, context);
-    console.log(`Page was succesfully downloaded into\n'${path}'`);
+    console.log(`Page was succesfully downloaded.\nPath is "${path}"`);
     return { filepath: path };
   } catch (error) {
-    console.error('Failed to save the file. Perhaps the directory does not exist');
+    console.error('Failed to save the file.\nPerhaps the directory does not exist');
     return error;
   }
 };
