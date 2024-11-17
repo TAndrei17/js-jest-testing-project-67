f5257580a89325aababd289f6e81cd576f01cdc1
Andrei Trunkin <inboxavt@gmail.com>
2024-11-17 18:03:41 +0100

    wip/add files stage 4

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import debug from 'debug';
+import axios from 'axios';
+import axiosDebugLog from 'axios-debug-log';
 import pageLoader from './bin/page-loader.js';
 
+const logPageLoader = debug('page-loader');
+axiosDebugLog(axios);
+
+export { logPageLoader };
 export default pageLoader;
